c8e902391b48b4b7e86a0155d5c8621e946c3297
Adraff <99280376+Adraff@users.noreply.github.com>
2025-07-20 03:49:50 -0300

    Update script.js

--- counter/script.js
+++ counter/script.js
@@ -1,6 +1,17 @@
 const addBtn = document.getElementById("addBtn");
 const subBtn = document.getElementById("subBtn");
+
+const addBtnBajos = document.getElementById("addBtnBajos");
+const subBtnBajos = document.getElementById("subBtnBajos");
+
+const addBtnAumentos = document.getElementById("addBtnAumentos");
+const subBtnAumentos = document.getElementById("subBtnAumentos");
+
 const puntosDobles = document.getElementById("puntosDobles");
+
+const puntosBajos = document.getElementById("puntosBajos");
+
+const puntosAumentos = document.getElementById("puntosAumentos");
 
 addBtn.addEventListener("click", () => {
     let numero = parseInt(puntosDobles.textContent);
@@ -23,4 +34,54 @@ subBtn.addEventListener("click", () => {
 
 
     
+});
+
+//--------------------------------------------------
+
+addBtnBajos.addEventListener("click", () => {
+    let numero = parseInt(puntosBajos.textContent);
+
+    numero++;
+
+    puntosBajos.textContent = numero;
+
+    
+
+});
+
+subBtnBajos.addEventListener("click", () => {
+    let numero = parseInt(puntosBajos.textContent);
+
+    if (numero > 0) {
+        numero--;
+        puntosBajos.textContent = numero;
+    }
+
+    
+
+});
+
+//------------------------------------------------------
+
+addBtnAumentos.addEventListener("click", () => {
+    let numero = parseInt(puntosAumentos.textContent);
+
+    numero++;
+
+    puntosAumentos.textContent = numero;
+
+    
+
+});
+
+subBtnAumentos.addEventListener("click", () => {
+    let numero = parseInt(puntosAumentos.textContent);
+
+    if (numero > 0) {
+    numero--;
+    puntosAumentos.textContent = numero;
+    }
+
+    
+
 });
